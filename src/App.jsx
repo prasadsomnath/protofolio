87555557241c4c513e448072ab363d616b70199c
prasad <prasadsomnath323@gmail.com>
2025-08-16 23:43:05 +0530

    update skills path

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
-import Skills from './components/skills';
 import Certificates from './components/Certificates';
 import Contact from './components/Contact';
+import Skills from './components/Skills';
 import Footer from './components/Footer';
 
 export default function App() {
