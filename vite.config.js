2c6001a88574d60d83f0077895fb1e962efdca93
prasad <prasadsomnath323@gmail.com>
2025-08-16 23:30:47 +0530

    deployment change

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_PATH || '/protofolio',
   server: {
-    host: '0.0.0.0',
-    allowedHosts: ['localhost', 'b90d6f7d77c1.ngrok-free.app'],
-    // The above list can include all hosts you want to allow
-  },
+    open: true
+  }
 });
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     allowedHosts: ['localhost', 'b90d6f7d77c1.ngrok-free.app'],
+//     // The above list can include all hosts you want to allow
+//   },
+// });
